8e6b79e87666a199072816878141ceb106a8f5a3
Srinjana Sahana <srinjanasahana@gmail.com>
2026-06-17 19:08:41 +0530

    Change API URL to production
    Updated API URL to use production endpoint.

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export const StoreContext=createContext(null)
 const StoreContextProvider =(props)=>{
    const[cartItems,setCartItems]=useState({});
-   const url = "http://localhost:4000";
+   const url = "https://food-website-1-s1oz.onrender.com";
    const[token,setToken]=useState("");
    const[foodList,setFoodList]=useState([]);
    const addToCart=async (itemId)=>{
